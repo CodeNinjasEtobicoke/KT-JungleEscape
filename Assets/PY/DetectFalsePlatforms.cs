3e2929c0d5e7c3559d9fd1b173d2b8e6f417a4b1
CodeNinjasEtobicoke <astrobrandon2013@gmail.com>
2025-04-24 17:30:44 -0400

    done

--- Assets/PY/DetectFalsePlatforms.cs
+++ Assets/PY/DetectFalsePlatforms.cs
@@ -7,6 +7,22 @@ public class DetectFalsePlatforms : MonoBehaviour
 
     void Update()
     {
+        float dist = 2f;
 
+        Vector3 pos = transform.position;
+        pos.y -= 0.5f;
+
+        bool hit = Physics.Raycast(pos, transform.forward, dist, 1 << 8);
+        Debug.DrawRay(pos, transform.forward * dist, Color.red);
+
+        if (hit == true)
+        {
+            Debug.LogWarning("Be careful");
+        }
+        else
+        {
+            Debug.Log("All clear");
+        }
+        
     }
 }
